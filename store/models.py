099fd8903383c7ed3cf8a3a1e1c831cdb56ddaa0
Nicole Monari <nicole.monari@strathmore.edu>
2024-11-21 23:43:52 +0300

    added more field for the customer

--- store/models.py
+++ store/models.py
@@ -5,16 +5,18 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=255)  #customer's full name
     email = models.EmailField(unique=True)  #this  each email is unique
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.name  # Display name when object is queried
+        return self.name 
 
 class Order(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE, related_name="orders"
-    )  # A customer can have multiple orders
-    order_date = models.DateTimeField(auto_now_add=True)  # Auto-set to now when created
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Total cost
+    )  #this ensures a customer can have multiple orders
+    order_date = models.DateTimeField(auto_now_add=True)  
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)  #total amount
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer.name}"
